44fd512836eb9150030270bad667f4920ba32556
Will Jones <will@sacharissa.co.uk>
2013-02-04 15:26:49 +0000

    Add the EA message and tidy up formatting.

--- src/Network/IRC/P10/Syntax.hs
+++ src/Network/IRC/P10/Syntax.hs
@@ -5,20 +5,27 @@ module Network.IRC.P10.Syntax where
 import Network.IRC.P10.Numeric
 
 data Message
-  = PassM     { _mPass            :: String
-              }
+  = PassM
+      { _mPass            :: String
+      }
 
-  | ServerM   { _mServerName      :: String
-              , _mBootTimestamp   :: String
-              , _mLinkTimestamp   :: String
-              , _mProtocol        :: String
-              , _mServerNumeric   :: Numeric ServerT
-              , _mMaxConnections  :: Int
-              , _mFlags           :: Maybe String
-              , _mDescription     :: String
-              }
+  | ServerM
+      { _mServerName      :: String
+      , _mBootTimestamp   :: String
+      , _mLinkTimestamp   :: String
+      , _mProtocol        :: String
+      , _mServerNumeric   :: Numeric ServerT
+      , _mMaxConnections  :: Int
+      , _mFlags           :: Maybe String
+      , _mDescription     :: String
+      }
 
-  | EndBurstM { _mServerNumeric   :: Numeric ServerT
-              }
+  | EndBurstM
+      { _mServerNumeric   :: Numeric ServerT
+      }
+
+  | EndBurstAckM
+      { _mServerNumeric   :: Numeric ServerT
+      }
 
   deriving (Eq, Show)
